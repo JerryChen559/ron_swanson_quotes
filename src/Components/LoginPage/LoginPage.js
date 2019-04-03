fdfd671cada73747f687af1aa9579ad72497f0a0
Jerry Chen <zgchen559@gmail.com>
2019-04-02 17:29:47 -0700

    LoginPage and QuotesPage, page logic setup

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
-import Navbar from "../Navbar/Navbar";
 import "./LoginPage.css";
 // import axios from "axios";
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.loginClick = this.login.bind(this);
+    this.registerClick = this.register.bind(this);
+  }
+
+  changeHandler(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  loginClick() {}
+  registerClick() {}
+
   render() {
+    console.log(this.state);
+
     return (
       <div>
-        <Navbar />
-        <p>Test LoginPage</p>
+        <h1>Swanson's Words of Wisdom</h1>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.changeHandler}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="text"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.changeHandler}
+          />
+        </div>
+        <div>
+          <button onClick={this.loginClick}>Login</button>
+          <button onClick={this.registerClick}>Register</button>
+        </div>
       </div>
     );
   }
--- src/Components/QuotesPage/QuotesPage.js
+++ src/Components/QuotesPage/QuotesPage.js
@@ -1,14 +1,86 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar/Navbar";
 import "./QuotesPage.css";
-// import axios from "axios";
+import axios from "axios";
 
 class QuotesPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteSize: "small",
+      displayQuote: "",
+      quoteRating: 0
+    };
+    this.onRadioChange = this.onRadioChange.bind(this);
+  }
+
+  // componentDidMount() {
+  // get generated quote's average stars
+  // }
+
+  onRadioChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  getQuote() {
+    if (this.state.quoteSize === "small") {
+      axios
+        .get("/api/getSmallQuote")
+        .then(res => {
+          // console.log(res.data)
+          this.setState({
+            displayQuote: res.data[0]
+          });
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
   render() {
+    console.log(this.state);
+
+    let message = null;
+    if (this.state.quoteSize === "small") {
+      message = "Quotes with 4 words or less";
+    }
+    if (this.state.quoteSize === "medium") {
+      message = "Quotes with 5 to 12 words";
+    }
+    if (this.state.quoteSize === "large") {
+      message = "Quotes with 13 words or more";
+    }
+
     return (
       <div>
-        <Navbar />
-        <p>Test QuotesPage</p>
+        <Navbar history={this.props.history} />
+        <p>Quote Size:</p>
+        <div>
+          <input
+            type="radio"
+            name="quoteSize"
+            value="small"
+            onChange={this.onRadioChange}
+          />
+          Small
+          <input
+            type="radio"
+            name="quoteSize"
+            value="medium"
+            onChange={this.onRadioChange}
+          />
+          Medium
+          <input
+            type="radio"
+            name="quoteSize"
+            value="large"
+            onChange={this.onRadioChange}
+          />
+          Large
+        </div>
+        <p>Note: {message}</p>
+        <div>
+          <button onClick={() => this.getQuote}>Generate Quote!</button>
+        </div>
       </div>
     );
   }
