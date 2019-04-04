a21cf7f1cd93d86503a821cd2cf5189f2b832d9b
Jerry Chen <zgchen559@gmail.com>
2019-04-03 20:10:20 -0700

    material UI Navbar. Logout logic!

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,57 @@
 import React, { Component } from "react";
-import "./Navbar.css";
-// import axios from "axios";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+// material UI
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  }
+};
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  logout() {
+    axios.get("/api/logout").then(res => {
+      console.log(res.data);
+      this.props.history.push("/");
+    });
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <p>THIS IS NAVBAR</p>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Ron Swanson Quotes :D
+            </Typography>
+            <Button color="inherit" onClick={() => this.logout()}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navbar);
